ecd2d81490aac588df45ce70964b47e60964c5f2
Roy Chan <chukinchan@gmail.com>
2021-09-19 21:11:28 +0800

    ADD: Q2d london baseline

--- Assignments/Assignment 5/student-new/src/london_baseline.py
+++ Assignments/Assignment 5/student-new/src/london_baseline.py
@@ -2,3 +2,7 @@
 #   example in the dev set.
 # Hint: Make use of existing code.
 # Your solution here should only be a few lines.
+import utils
+
+total, correct = utils.evaluate_places('birth_dev.tsv', ['London']*500)
+print('Correct: {} out of {}: {}%'.format(correct, total, correct/total*100))
